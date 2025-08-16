40cbda9a8b3cdb62677e7a3f780475b2cfdf9bce
ronyginosar <rony.ginosar@gmail.com>
2025-08-16 20:33:08 +0300

    initial aliasing

--- sketch.js
+++ sketch.js
@@ -21,6 +21,11 @@ let FIVE_SECONDS = 60*5;
 let TEN_SECONDS = 60*10;
 let TWO_MINUTES = 60*2*60; // 2 minutes
 
+
+let phase = 0;     // shifting angle for animation
+let phaseSpeed = 0.05; // how fast the wave moves
+
+
 function preload(){
   // TODO
     // audiofile = loadSound('/assets/Alto_score_simulation-for-rony_5th-movement.wav');
@@ -96,10 +101,100 @@ function draw() {
     }
 
     // TODO draw sketch
-
-
     reset(); // clean canvas
+
+
+
+    
   }
+
+  // test
+  stroke("black");
+  strokeWeight(1);
+  let colors = ["red", "green", "blue"]; // or try CMYK!
+    let spacing = 1;
+
+
+
+    for (let i = 0; i < height/2; i+=spacing) {
+        // line(i, 0, i, height);
+            let c = colors[(i / spacing) % colors.length]; // cycle through colors
+        stroke(c);
+        line(0, i, width, i);
+        // // maybe zigzag is sound wave?
+        // var waveform = fft.waveform();
+        // for (var i = 0; i < waveform.length; i++) {
+        //     var x = map(i, 0, waveform.length, 0, width);
+        //     var y = map(waveform[i], -1, 1, height, 0);
+            // ellipse(x, y, 5, 5);
+            // ellipse(i, i, 5, 5);
+        // }
+
+        // if (i % 5 == 0) { // space out zigz
+        // // zigzag brut force
+        // let yAmp = 9;
+        // let  y = -yAmp;
+        // noFill();
+        // stroke("black");
+        // push();
+        //   // translate(0, frameRate());
+        //   // TODO move zigzag
+        //   beginShape();
+        //   for (let j = 0; j < 100; j++) {
+        //     vertex(j*10, y+i);
+        //     if(y == -yAmp) {
+        //       y = yAmp;
+        //     } else {
+        //       y = -yAmp;
+        //     }
+        //   }
+        //   endShape();
+        // pop();
+        // }
+
+
+    }
+
+    // for (let i = 0; i < height/2; i+=5) { // space out zigz
+    //     // zigzag brut force
+    //     let yAmp = 9;
+    //     let  y = -yAmp;
+    //     noFill();
+    //     stroke("black");
+    //     // push();
+    //       // translate(0, frameRate());
+    //       // TODO move zigzag
+    //       beginShape();
+    //       for (let j = 0; j < 100; j++) {
+    //         vertex(j*10, y+i);
+    //         if(y == -yAmp) {
+    //           y = yAmp;
+    //         } else {
+    //           y = -yAmp;
+    //         }
+    //       }
+    //       endShape();
+    //     // pop();
+    //   }
+
+
+let xAmp = 10;     // amplitude of zigzag
+  let xFreq = 0.05;  // frequency of zigzag
+  // y+= 2 to 8 works good
+  for (let y = 0; y < height/2; y += 8) {
+    stroke(0, 180); // semi-transparent black
+    beginShape();
+    noFill();
+    for (let x = 0; x < width; x += 5) {
+      let yOffset = sin(x * xFreq ) * xAmp;
+      vertex(x, y + yOffset);
+    }
+    endShape();
+  }
+
+  // update phase each frame
+  // phase += phaseSpeed;
+
 }
 
 function doubleClicked() {
